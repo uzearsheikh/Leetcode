3d5e0b564db47919842473acc13e9e31e71360ae
uzearsheikh <uzearsheikh24@gmail.com>
2026-03-02 14:58:51 +0530

    Time: 1 ms (7.96%), Space: 19.3 MB (57.89%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rec(self, i, nums, dp):
+        if i >= len(nums):
+            return 0
+        
+        if dp[i] != -1:
+            return dp[i]
+        
+        take = nums[i] + self.rec(i+2, nums, dp)
+        not_take = self.rec(i+1, nums, dp)
+        
+        dp[i] = max(take, not_take)
+        return dp[i]
+
+    def rob(self, nums: List[int]) -> int:
+        dp = [-1] * len(nums)
+        return self.rec(0, nums, dp)
