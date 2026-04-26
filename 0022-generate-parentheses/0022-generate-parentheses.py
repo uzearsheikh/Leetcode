5f94c30708e9993791247856649e9110512e38c4
uzearsheikh <uzearsheikh24@gmail.com>
2026-04-26 08:05:11 +0530

    Time: 0 ms (100%), Space: 19.6 MB (11.68%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        sol = []
+
+        def backtrack(open , close):
+            if len(sol) == 2*n:
+                res.append("".join(sol))
+                return
+            
+            if open<n:
+                sol.append("(")
+                backtrack(open+1,close)
+                sol.pop()
+            if open>close:
+                sol.append(")")
+                backtrack(open,close+1)
+                sol.pop()
+        backtrack(0,0)
+        return res
