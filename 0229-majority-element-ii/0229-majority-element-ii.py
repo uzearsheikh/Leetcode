db04394560c65a91b3e0b10f1d9995a72c52aae6
uzearsheikh <uzearsheikh24@gmail.com>
2026-07-09 10:32:29 +0530

    Time: 2 ms (94.51%), Space: 23.8 MB (8.95%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,15 +1,16 @@
 from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        hashmap = {}
-        n = len(nums)
-        for i in nums:
-            if i not in hashmap:
-                hashmap[i] =1
-            else:
-                hashmap[i]+=1
+        # hashmap = {}
+        # n = len(nums)
+        # for i in nums:
+        #     if i not in hashmap:
+        #         hashmap[i] =1
+        #     else:
+        #         hashmap[i]+=1
         ans = []
-        freq = Counter(hashmap)
+        n = len(nums)
+        freq = Counter(nums)
         for i in freq:
             if freq[i]> n//3:
                 ans.append(i)
