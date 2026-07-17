d51503326adbc4710a9ee92031ad92e86d6095b6
uzearsheikh <uzearsheikh24@gmail.com>
2026-07-17 09:09:05 +0530

    Time: 667 ms (27.4%), Space: 31.6 MB (48.96%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,9 +1,22 @@
 class Solution:
-  def change(self, amount: int, coins: list[int]) -> int:
-    dp = [1] + [0] * amount
+    def rec(self,i,amount,coins , dp):
+        if amount == 0:
+            return 1
+        if i == len(coins):
+            return 0
+        if dp[i][amount]!=-1:
+            return dp[i][amount]
+        
+        take = 0
+        if coins[i] <= amount:
+            take = self.rec(i, amount - coins[i], coins , dp)
+        
+        not_take = self.rec(i+1,amount,coins , dp)
 
-    for coin in coins:
-      for i in range(coin, amount + 1):
-        dp[i] += dp[i - coin]
+        dp[i][amount] = take + not_take
+        return dp[i][amount]
 
-    return dp[amount]
+    def change(self, amount: int, coins: list[int]) -> int:
+        dp = [[-1] * (amount+1) for _ in range(len(coins))]
+        return self.rec(0, amount, coins,dp)
+    
