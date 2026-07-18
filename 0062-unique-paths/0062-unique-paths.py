f355a37be2851c9142f75d9abf2ce80b90659462
uzearsheikh <uzearsheikh24@gmail.com>
2026-07-18 09:26:43 +0530

    Time: 3 ms (19.96%), Space: 19.1 MB (99.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,11 +1,18 @@
 class Solution:
+    def rec(self,i,j,m,n,dp):
+        if i == m-1 and j == n-1:
+            return 1
+        # Grid ke bahar chale gaye
+        if i >= m or j >= n:
+            return 0
+        if dp[i][j]!=-1:
+            return dp[i][j]
+        up = self.rec(i+1,j,m,n,dp)
+        down = self.rec(i,j+1,m,n,dp)
+        dp[i][j] = up+down
+        return dp[i][j]
     def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[-1]*(n+1) for _ in range(m+1)]
+        return self.rec(0,0,m,n,dp)
         
-        dp = [[1]*n for _ in range(m)]
-        
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        
-        return dp[m-1][n-1]
         
