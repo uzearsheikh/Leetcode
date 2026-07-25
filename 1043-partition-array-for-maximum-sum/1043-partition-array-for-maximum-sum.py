1b43e4d6521cd7fadcafd45534bd3f34edb5fc0f
uzearsheikh <uzearsheikh24@gmail.com>
2026-07-25 11:17:57 +0530

    Time: 255 ms (23.55%), Space: 19.4 MB (41.32%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rec(self,i,arr,k,dp):
+        n = len(arr)
+        if i == n:
+            return 0
+        if dp[i]!=-1:
+            return dp[i]
+        ans = 0
+        maxi = 0
+        for end in range(i, min(n, i+k)):     #min islie iy ahai jisse list out of bound na aye 
+            maxi = max(maxi , arr[end])
+            length = end - i + 1
+            current = length * maxi
+            ans = max(ans,current + self.rec(end+1,arr,k,dp))
+        
+        dp[i] = ans
+        return dp[i]
+
+
+
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        n = len(arr)
+        dp = [-1]*n
+        return self.rec(0,arr,k,dp)       
