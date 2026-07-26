69809d45a83f8b7ce5fbbf199d334413f7c66e07
uzearsheikh <uzearsheikh24@gmail.com>
2026-07-26 16:13:57 +0530

    Time: 59 ms (12.11%), Space: 34.1 MB (58.13%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,27 +1,32 @@
 class Solution:
-    def partition(self, s):
+    def isPalindrome(self, start, end, s):
+        return s[start:end+1] == s[start:end+1][::-1]
+
+    def partition(self, s: str):
         res = []
         sol = []
-        
-        def isPalindrome(sub):
-            return sub == sub[::-1]
-        
-        def backtrack(start):
-            # base condition
-            if start == len(s):
-                res.append(sol[:])
+
+        def rec(i):
+
+            # Base Case
+            if i == len(s):
+                res.append(sol.copy())
                 return
-            
-            for end in range(start, len(s)):
-                if isPalindrome(s[start:end+1]):
-                    
-                    # TAKE
-                    sol.append(s[start:end+1])
-                    
-                    backtrack(end + 1)
-                    
-                    # NOT TAKE (backtrack)
+
+            # Try every possible partition
+            for end in range(i, len(s)):
+
+                # Valid partition?
+                if self.isPalindrome(i, end, s):
+
+                    # Choose
+                    sol.append(s[i:end+1])
+
+                    # Explore
+                    rec(end + 1)
+
+                    # Undo
                     sol.pop()
-        
-        backtrack(0)
+
+        rec(0)
         return res
