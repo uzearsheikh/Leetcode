6c2053d89e55f189f8e82521a9621cba47370bf5
uzearsheikh <uzearsheikh24@gmail.com>
2026-09-18 11:02:40 +0530

    Time: 0 ms (100%), Space: 42.4 MB (5.06%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,29 @@
 class Solution {
+
+    public int fibo(int n, int[] dp) {
+
+        if (n <= 1) {
+            return n;
+        }
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
+
+        return dp[n];
+    }
+
     public int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-2)+fib(n-1);
-        
+
+        int[] dp = new int[n + 1];
+
+        // -1 se initialize
+        for (int i = 0; i <= n; i++) {
+            dp[i] = -1;
+        }
+
+        return fibo(n, dp);
     }
 }
